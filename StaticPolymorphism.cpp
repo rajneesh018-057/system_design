33fd36570b5030ba79641d28afa1b0c7ae394ab4
rajneesh018-057 <rajneeshkumar3107@gmail.com>
2026-01-18 17:48:26 +0530

    add some basic operation static polymorphism
    Removed comment explaining static polymorphism.

--- StaticPolymorphism.cpp
+++ StaticPolymorphism.cpp
@@ -3,13 +3,7 @@
 
 using namespace std;
 
-/*
-Static Polymorphism (Compile-time polymorphism) in real life says that
-the same action can behave differently depending on the input parameters.
-For example, a Manual car can accelerate by a fixed amount or by a
-specific amount you request. In programming, we achieve this via method
-overloading: multiple methods with the same name but different signatures.
-*/
+
 
 class ManualCar {
 
